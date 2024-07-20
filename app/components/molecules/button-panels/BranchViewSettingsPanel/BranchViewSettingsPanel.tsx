ad40b0d6b55004c5412bcaa4e443c83483365108
Leonid Vilensky <leonid.vilensky@similarweb.com>
2024-07-20 13:10:38 +0300

    Restructured components, Added BaseUrl & Paths to tscofig

---
+++ app/components/molecules/button-panels/BranchViewSettingsPanel/BranchViewSettingsPanel.tsx
@@ -0,0 +1,7 @@
+export const BranchViewSettingsPanel = () => {
+	return (
+		<div>
+			<h1>BranchViewSettingsPanel</h1>
+		</div>
+	);
+}
---
+++ app/components/organisms/view-sections/CentralSection/CentralSection.tsx
@@ -0,0 +1,3 @@
+export const CentralSection = () => {
+	return <>CentralPanel</>
+}
